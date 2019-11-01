cfda08883ca7815c9646697030c63fa035d9bc97
lalumiere82 <lalumiere.andre@gmail.com>
2019-11-01 08:57:43 -0700

    Uploaded project to tgithub

---
+++ DocusignPlanners/DocusignPlanners/DataValues.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocusignPlanners
+{
+    public class DataValues
+    {
+        //Constructors for DataValues list
+        public string VendorPartner { get; set; }
+        public string BaseMembership { get; set; }
+        public string PrintCircular { get; set; }
+        public string ProMonthlyBuys { get; set; }
+        public string TradeAdvertisments { get; set; }
+        public string AchieversClubSponsorship { get; set; }
+        public string TotalPromotionalBudget { get; set; }
+        public string Notes { get; set; }
+        public string TotalPromotionalBudgetSmall { get; set; }
+        public string ContactName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
